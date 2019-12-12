3eda6c5bd83e4917f4cfa06cab59f53ffe03c8fb
Mingi Jo <guny524@gmail.com>
2019-12-12 14:47:19 +0900

    kbhit 추가

--- game_client.c
+++ game_client.c
@@ -1,6 +1,78 @@
 #include<stdio.h>
 #include<stdlib.h>	//rand(), srand();
 #include<time.h>	//time(NULL);
+#include<termios.h>	//struct tertmios, tcgetattr(), tcsetattr();
+#include<unistd.h>	//
+#include<fcntl.h>	//fcntl();
+static struct termios old, current;
+
+void clear(char arr[MAP_ROW][MAP_COL])
+{
+	for (int i = 0; i < MAP_COL; i++)
+		for (int j = 0; j < MAP_ROW; j++)
+			arr[i][j] = 0;
+}
+void display(char arr[MAP_ROW][MAP_COL])
+{
+	frame_change(arr);
+}
+void waitm()
+{
+	usleep(100000);		//1.5s
+}
+void wait()
+{
+	usleep(1500000);	//100ms
+}
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) {
+	struct termios old, current;
+	tcgetattr(0, &old); /* grab old terminal i/o settings */
+	current = old; /* make new settings same as old settings */
+	current.c_lflag &= ~ICANON; /* disable buffered i/o */
+	if (echo) {
+		current.c_lflag |= ECHO; /* set echo mode */
+	}
+	else {
+		current.c_lflag &= ~ECHO; /* set no echo mode */
+	}
+	tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+} /* Read 1 character - echo defines echo mode */
+char getch() {
+	char ch;
+	struct termios old;
+	tcgetattr(STDIN_FILENO, &old);
+	initTermios(0);	//echo 0
+	ch = getchar();
+	tcsetattr(0, TCSANOW, &old);	//reset Termios
+	return ch;
+}
+int kbhit(void)
+{
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	if (ch != EOF)
+	{
+		ungetc(ch, stdin);
+		return 1;
+	}
+
+	return 0;
+}
 
 typedef struct Coordinate {
 	int row, col;
@@ -9,12 +81,6 @@ typedef struct Snake {
 	coor arr[MAP_COL * MAP_ROW];
 	int size;
 }snake;
-
-void run_snake();
-
-#include"display.h"
-#include"snake.h"
-#include"frame.h"
 
 char key = 0;
 snake s = { 0, };
