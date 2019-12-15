df84c93d8c98536db03a43b907ac864320c8c85f
Mingi Jo <guny524@gmail.com>
2019-12-15 19:44:55 +0900

    게임 서버 하나로 클라이언트 2개 완성

--- game_client.c
+++ game_client.c
@@ -1,31 +1,27 @@
 #include<stdio.h>
-#include<stdlib.h>	//rand(), srand();
-#include<time.h>	//time(NULL);
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<pthread.h>
+#include<mqueue.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<assert.h>
+#include<time.h>
 #include<termios.h>	//struct tertmios, tcgetattr(), tcsetattr();
-#include<unistd.h>	//
-#include<fcntl.h>	//fcntl();
-static struct termios old, current;
 
-void clear(char arr[MAP_ROW][MAP_COL])
-{
-	for (int i = 0; i < MAP_COL; i++)
-		for (int j = 0; j < MAP_ROW; j++)
-			arr[i][j] = 0;
-}
-void display(char arr[MAP_ROW][MAP_COL])
-{
-	frame_change(arr);
-}
-void waitm()
-{
-	usleep(100000);		//1.5s
-}
-void wait()
-{
-	usleep(1500000);	//100ms
-}
+#define GAME_PORT_NUM 41195
+
+#define MAP_COL 16
+#define MAP_ROW 16
+
 /* Initialize new terminal i/o settings */
-void initTermios(int echo) {
+void initTermios(int echo)
+{
 	struct termios old, current;
 	tcgetattr(0, &old); /* grab old terminal i/o settings */
 	current = old; /* make new settings same as old settings */
@@ -38,7 +34,8 @@ void initTermios(int echo) {
 	}
 	tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
 } /* Read 1 character - echo defines echo mode */
-char getch() {
+char getch()
+{
 	char ch;
 	struct termios old;
 	tcgetattr(STDIN_FILENO, &old);
@@ -73,170 +70,98 @@ int kbhit(void)
 
 	return 0;
 }
+static struct termios old, current;
 
-typedef struct Coordinate {
-	int row, col;
-}coor;
-typedef struct Snake {
-	coor arr[MAP_COL * MAP_ROW];
-	int size;
-}snake;
+void display(char arr[MAP_COL][MAP_ROW])
+{
+    for(int i=0;i<MAP_COL;i++)
+    {
+        for(int j=0;j<MAP_ROW;j++)
+        {
+            if(arr[i][j]==0)
+                printf("  ");
+            else
+                printf("ㅁ");
+        }
+		printf("\n");
+    }
+}
+void client(char *ip_address)
+{
+    //소켓
+    int sock;
+    struct sockaddr_in serv_addr;
+    int str_len;
+	int key = 's';
+	char arr[MAP_COL][MAP_ROW]={0,};
 
-char key = 0;
-snake s = { 0, };
-coor feed = { 0, };
-snake tmp_s = { 0, };
+    //소켓 열기
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        perror("socket() error");
+        exit(1);
+    }
 
-char arr[MAP_COL][MAP_ROW] = { 0, };
-char map[MAP_COL][MAP_ROW] = { 0, };
+    //클라이언트 주소 초기화
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(ip_address);
+    serv_addr.sin_port = htons(GAME_PORT_NUM);
 
-void init_map()
-{
-	for (int i = 0; i < MAP_COL; i++)
-		for (int j = 0; j < MAP_ROW; j++)
-			map[i][j] = 0;
+    //서버와 연결
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+    {
+        perror("connect() error");
+        exit(1);
+    }
+	system("clear");
 
-	for (int i = 0; i < MAP_COL; i++)
-		map[i][0] = 1;
-	for (int i = 0; i < MAP_COL; i++)
-		map[i][MAP_ROW - 1] = 1;
-	for (int i = 0; i < MAP_ROW; i++)
-		map[0][i] = 1;
-	for (int i = 0; i < MAP_ROW; i++)
-		map[MAP_COL - 1][i] = 1;
-}
-void init_snake()
-{
-	s.arr[0].col = rand() % (MAP_COL - 2) + 1;
-	s.arr[0].row = rand() % (MAP_ROW - 2) + 1;
-	s.size = 1;
-}
-void merge_arr()
-{
-	for (int i = 0; i < MAP_COL; i++)
-		for (int j = 0; j < MAP_ROW; j++)
-			arr[i][j] = map[i][j];	//맵 표시
-	for (int i = 0; i < s.size; i++)
-		arr[s.arr[i].col][s.arr[i].row] = 1;
-	arr[feed.col][feed.row] = 1;
-}
-void copy()
-{
-	tmp_s.size = s.size;
-	for (int i = 0; i < s.size; i++)
-		tmp_s.arr[i] = s.arr[i];
-}
-void move(snake *ss, char key)	//꼬리칸 머리에서 키보드 방향으로 이동
-{
-	for (int i=ss->size-2;i>=0;i--)	//머리랑 꼬리만 이동 나머진 그냥 쉬프트
-		ss->arr[i+1] = ss->arr[i];
-	switch(key)	//머리 이동
-	{
-	case 'a':
-		ss->arr[0].row--;
-		break;
-	case 'w':
-		ss->arr[0].col--;
-		break;
-	case 's':
-		ss->arr[0].col++;
-		break;
-	case 'd':
-		ss->arr[0].row++;
-		break;
-	}
-}
-int is_crash(snake *ss)	//with map?
-{
-	for (int i = 0; i < ss->size; i++)
-		if (ss->arr[i].row == 0 || ss->arr[i].row == MAP_ROW-1 || ss->arr[i].col == 0 || ss->arr[i].col == MAP_COL-1)
-			return 1;
-	return 0;
-}
-int is_collide(snake *ss)	//it self?
-{
-	for (int i = 0; i < ss->size; i++)
-		for (int j = 0; j < i; j++)
-			if ((ss->arr[i].row == ss->arr[j].row) && (ss->arr[i].col == ss->arr[j].col))
-				return 1;
-	return 0;
-}
-int is_consume(snake *ss)	//with feed?
-{
-	for (int i = 0; i < ss->size; i++)
-		if (ss->arr[i].row == feed.row && ss->arr[i].col == feed.col)
-			return 1;
-	return 0;
-}
-void init_feed(snake *ss)
-{
-	feed.col = rand() % (MAP_COL - 2) + 1;
-	feed.row = rand() % (MAP_ROW - 2) + 1;
-	if (is_consume(ss))
-		init_feed(ss);
-}
-void finish()
-{
-	clear(arr);
-	display(arr);
-	for (int i=0;i<MAP_ROW;i++)
-	{
-		for (int j=0;j<MAP_COL;j++)
-		{
-			arr[i][j] = 1;
-			display(arr);
-		}
-	}
-}
-void run_snake()
-{
-	int first = 1;
-	srand((unsigned int)time(NULL));
-
-	key = 0;
-
-	init_map();
-	init_snake();
-	init_feed(&s);
-
-	merge_arr();
-	display(arr);
-
-	while (1)
-	{
-		wait();
-		if (kbhit())
+    while(1)
+    {
+        if(kbhit())
 		{
 			key = getch();
 			while (kbhit())
 				getch();
 		}
-		if (key == 0x1b)	//ESC
-			break;
-		else if (key == 0 && first)
-			first = 0;
-		else
+        if(write(sock, &key, sizeof(int))==-1)
+        {
+            perror("write");
+            exit(1);
+        }
+        if(read(sock, (char*)&arr, sizeof(char)*MAP_COL*MAP_ROW) <= 0)
 		{
-			copy();
-			move(&tmp_s, key);
-
-			if (is_crash(&tmp_s) || is_collide(&tmp_s))
-			{
-				finish();
-				return;
-			}
-			else if (is_consume(&tmp_s))
-			{
-				coor tmp = s.arr[s.size-1];
-				move(&s, key);
-				s.arr[s.size++] = tmp;
-				init_feed(&s);
-			}
-			else
-				move(&s, key);
-
-			merge_arr();
-			display(arr);
+			perror("read");
 		}
-	}
+
+		if(arr[0][0] == -2)
+		{
+			printf("WIN\n");
+			break;
+		}
+		else if(arr[0][0] == -3)
+		{
+			printf("LOSE\n");
+			break;
+		}
+        system("clear");
+		display(arr);
+    }
+
+    close(sock);
+}
+int main(int argc, char ** argv)
+{
+    if(argc == 2)
+    {
+        client(argv[1]);
+    }
+    else
+    {
+        printf("Usage client: %s <IP>\n", argv[0]);
+        exit(1);
+    }
+
+    return 0;
 }
--- game_server.c
+++ game_server.c
@@ -1,31 +1,30 @@
 #include<stdio.h>
-#include<stdlib.h>	//rand(), srand();
-#include<time.h>	//time(NULL);
-#include<termios.h>	//struct tertmios, tcgetattr(), tcsetattr();
-#include<unistd.h>	//
-#include<fcntl.h>	//fcntl();
-static struct termios old, current;
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 
-void clear(char arr[MAP_ROW][MAP_COL])
-{
-	for (int i = 0; i < MAP_COL; i++)
-		for (int j = 0; j < MAP_ROW; j++)
-			arr[i][j] = 0;
-}
-void display(char arr[MAP_ROW][MAP_COL])
-{
-	frame_change(arr);
-}
-void waitm()
-{
-	usleep(100000);		//1.5s
-}
-void wait()
-{
-	usleep(1500000);	//100ms
-}
+#include<net/if.h>  //inet_ntop(), IFNAMSIZ
+#include<sys/ioctl.h>   //ioctl(), SIOCGIFADDR
+
+#include<pthread.h>
+#include<assert.h>
+
+#include<mqueue.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+#include<termios.h>	//struct tertmios, tcgetattr(), tcsetattr();
+
+#define IP_ITERFACE_NAME "wlp5s0"
+#define GAME_PORT_NUM 41195
+
 /* Initialize new terminal i/o settings */
-void initTermios(int echo) {
+void initTermios(int echo)
+{
 	struct termios old, current;
 	tcgetattr(0, &old); /* grab old terminal i/o settings */
 	current = old; /* make new settings same as old settings */
@@ -38,7 +37,8 @@ void initTermios(int echo) {
 	}
 	tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
 } /* Read 1 character - echo defines echo mode */
-char getch() {
+char getch()
+{
 	char ch;
 	struct termios old;
 	tcgetattr(STDIN_FILENO, &old);
@@ -73,6 +73,10 @@ int kbhit(void)
 
 	return 0;
 }
+static struct termios old, current;
+
+#define MAP_COL 16
+#define MAP_ROW 16
 
 typedef struct Coordinate {
 	int row, col;
@@ -82,161 +86,336 @@ typedef struct Snake {
 	int size;
 }snake;
 
-char key = 0;
-snake s = { 0, };
-coor feed = { 0, };
-snake tmp_s = { 0, };
+int finished = 0;
 
-char arr[MAP_COL][MAP_ROW] = { 0, };
-char map[MAP_COL][MAP_ROW] = { 0, };
-
-void init_map()
+void wait()
+{
+	usleep(1000000);
+}
+void display(char arr[MAP_COL][MAP_ROW])
+{
+    for(int i=0;i<MAP_COL;i++)
+    {
+        for(int j=0;j<MAP_ROW;j++)
+        {
+            if(arr[i][j]==0)
+                printf("  ");
+            else
+                printf("ㅁ");
+        }
+        printf("\n");
+    }
+}
+void init_map(char arr[MAP_COL][MAP_ROW])
 {
 	for (int i = 0; i < MAP_COL; i++)
 		for (int j = 0; j < MAP_ROW; j++)
-			map[i][j] = 0;
+			arr[i][j] = 0;
 
 	for (int i = 0; i < MAP_COL; i++)
-		map[i][0] = 1;
+		arr[i][0] = 1;
 	for (int i = 0; i < MAP_COL; i++)
-		map[i][MAP_ROW - 1] = 1;
+		arr[i][MAP_ROW - 1] = 1;
 	for (int i = 0; i < MAP_ROW; i++)
-		map[0][i] = 1;
+		arr[0][i] = 1;
 	for (int i = 0; i < MAP_ROW; i++)
-		map[MAP_COL - 1][i] = 1;
+		arr[MAP_COL - 1][i] = 1;
 }
-void init_snake()
+void init_snake(snake *s)
 {
-	s.arr[0].col = rand() % (MAP_COL - 2) + 1;
-	s.arr[0].row = rand() % (MAP_ROW - 2) + 1;
-	s.size = 1;
+	s->arr[0].col = rand() % (MAP_COL - 2) + 1;
+	s->arr[0].row = rand() % (MAP_ROW - 2) + 1;
+	s->size = 1;
 }
-void merge_arr()
+int is_killed(snake *s, snake *other)   //by_ohter?
+{
+    for (int i = 0; i < other->size; i++)
+		if(s->arr[0].row == other->arr[i].row && s->arr[0].col == other->arr[i].col)
+			return 1;
+    return 0;
+}
+void init_snake_avoid(snake *s, snake *avoid)
+{
+	s->arr[0].col = rand() % (MAP_COL - 2) + 1;
+	s->arr[0].row = rand() % (MAP_ROW - 2) + 1;
+	s->size = 1;
+    if(is_killed(s, avoid))
+        init_snake_avoid(s, avoid);
+}
+void merge_arr(char dest[MAP_COL][MAP_ROW], char map[MAP_COL][MAP_ROW], snake *s, snake *other, coor *f)
 {
 	for (int i = 0; i < MAP_COL; i++)
 		for (int j = 0; j < MAP_ROW; j++)
-			arr[i][j] = map[i][j];	//맵 표시
-	for (int i = 0; i < s.size; i++)
-		arr[s.arr[i].col][s.arr[i].row] = 1;
-	arr[feed.col][feed.row] = 1;
+			dest[i][j] = map[i][j];	//맵 표시
+	for (int i = 0; i < s->size; i++)
+		dest[s->arr[i].col][s->arr[i].row] = 1;
+    for (int i = 0; i < other->size; i++)
+        dest[other->arr[i].col][other->arr[i].row] = 1;
+	dest[f->col][f->row] = 1;
 }
-void copy()
+void copy(snake *dest, snake *s)
 {
-	tmp_s.size = s.size;
-	for (int i = 0; i < s.size; i++)
-		tmp_s.arr[i] = s.arr[i];
+	dest->size = s->size;
+	for (int i = 0; i < s->size; i++)
+		dest->arr[i] = s->arr[i];
 }
-void move(snake *ss, char key)	//꼬리칸 머리에서 키보드 방향으로 이동
+void move(snake *s, char key)	//꼬리칸 머리에서 키보드 방향으로 이동
 {
-	for (int i=ss->size-2;i>=0;i--)	//머리랑 꼬리만 이동 나머진 그냥 쉬프트
-		ss->arr[i+1] = ss->arr[i];
+	for (int i=s->size-2;i>=0;i--)	//머리랑 꼬리만 이동 나머진 그냥 쉬프트
+		s->arr[i+1] = s->arr[i];
 	switch(key)	//머리 이동
 	{
 	case 'a':
-		ss->arr[0].row--;
+		s->arr[0].row--;
 		break;
 	case 'w':
-		ss->arr[0].col--;
+		s->arr[0].col--;
 		break;
 	case 's':
-		ss->arr[0].col++;
+		s->arr[0].col++;
 		break;
 	case 'd':
-		ss->arr[0].row++;
+		s->arr[0].row++;
 		break;
 	}
 }
-int is_crash(snake *ss)	//with map?
+int is_crash(snake *s)	//with map?
 {
-	for (int i = 0; i < ss->size; i++)
-		if (ss->arr[i].row == 0 || ss->arr[i].row == MAP_ROW-1 || ss->arr[i].col == 0 || ss->arr[i].col == MAP_COL-1)
+	for (int i = 0; i < s->size; i++)
+		if (s->arr[i].row == 0 || s->arr[i].row == MAP_ROW-1 || s->arr[i].col == 0 || s->arr[i].col == MAP_COL-1)
 			return 1;
 	return 0;
 }
-int is_collide(snake *ss)	//it self?
+int is_collide(snake *s)	//it self?
 {
-	for (int i = 0; i < ss->size; i++)
+	for (int i = 0; i < s->size; i++)
 		for (int j = 0; j < i; j++)
-			if ((ss->arr[i].row == ss->arr[j].row) && (ss->arr[i].col == ss->arr[j].col))
+			if ((s->arr[i].row == s->arr[j].row) && (s->arr[i].col == s->arr[j].col))
 				return 1;
 	return 0;
 }
-int is_consume(snake *ss)	//with feed?
+int is_consume(snake *s, coor *f)	//with feed?
 {
-	for (int i = 0; i < ss->size; i++)
-		if (ss->arr[i].row == feed.row && ss->arr[i].col == feed.col)
+	for (int i = 0; i < s->size; i++)
+		if (s->arr[i].row == f->row && s->arr[i].col == f->col)
 			return 1;
 	return 0;
 }
-void init_feed(snake *ss)
+void init_feed(snake *s, snake *other, coor *f)
 {
-	feed.col = rand() % (MAP_COL - 2) + 1;
-	feed.row = rand() % (MAP_ROW - 2) + 1;
-	if (is_consume(ss))
-		init_feed(ss);
+	f->col = rand() % (MAP_COL - 2) + 1;
+	f->row = rand() % (MAP_ROW - 2) + 1;
+	if (is_consume(s, f) || is_consume(other, f))
+		init_feed(s, other, f);
 }
-void finish()
+void *cal(void *sockets)
 {
-	clear(arr);
-	display(arr);
-	for (int i=0;i<MAP_ROW;i++)
-	{
-		for (int j=0;j<MAP_COL;j++)
-		{
-			arr[i][j] = 1;
-			display(arr);
-		}
-	}
-}
-void run_snake()
-{
-	int first = 1;
+	int *sock;
+	int key='s', other_key='s';
+
+    char arr[MAP_COL][MAP_ROW] = { 0, };
+    char map[MAP_COL][MAP_ROW] = { 0, };
+
+    snake s = { 0, };
+    snake tmp_s = { 0, };
+    snake other_s = {0,};
+    snake tmp_other_s = {0,};
+    coor f = { 0, };
+
+	sock = (int*)sockets;
+
 	srand((unsigned int)time(NULL));
 
-	key = 0;
+	init_map(map);
+	init_snake(&s);
+    init_snake_avoid(&other_s,&s);
+	init_feed(&s,&other_s,&f);
 
-	init_map();
-	init_snake();
-	init_feed(&s);
-
-	merge_arr();
+	merge_arr(arr, map, &s, &other_s, &f);
+    system("clear");
 	display(arr);
 
 	while (1)
 	{
 		wait();
-		if (kbhit())
+		if((read(sock[0], &key, sizeof(int))) <= 0)
 		{
-			key = getch();
-			while (kbhit())
-				getch();
+			perror("read0");
+			exit(1);
 		}
-		if (key == 0x1b)	//ESC
+		if((read(sock[1], &other_key, sizeof(int))) <= 0)
+		{
+			perror("read1");
+			exit(1);
+		}
+
+		copy(&tmp_s, &s);
+        copy(&tmp_other_s, &other_s);
+
+		move(&tmp_s, key);
+        move(&tmp_other_s, other_key);
+
+		if (is_crash(&tmp_s) || is_collide(&tmp_s) || is_killed(&tmp_s, &tmp_other_s))
+		{
+            /*
+            if(is_crash(&tmp_s))
+                printf("s crash\n");
+            else if(is_collide(&tmp_s))
+                printf("s collide\n");
+            else if(is_killed(&tmp_s, &tmp_other_s))
+                printf("s killed\n");
+                */
+			for(int i=0;i<MAP_COL;i++)
+				for(int j=0;j<MAP_ROW;j++)
+					arr[i][j] = -2;	//win
+            write(sock[1], arr, sizeof(char)*MAP_COL*MAP_ROW);
+			for(int i=0;i<MAP_COL;i++)
+				for(int j=0;j<MAP_ROW;j++)
+					arr[i][j] = -3;	//lose
+			write(sock[0], arr, sizeof(char)*MAP_COL*MAP_ROW);
 			break;
-		else if (key == 0 && first)
-			first = 0;
-		else
-		{
-			copy();
-			move(&tmp_s, key);
-
-			if (is_crash(&tmp_s) || is_collide(&tmp_s))
-			{
-				finish();
-				return;
-			}
-			else if (is_consume(&tmp_s))
-			{
-				coor tmp = s.arr[s.size-1];
-				move(&s, key);
-				s.arr[s.size++] = tmp;
-				init_feed(&s);
-			}
-			else
-				move(&s, key);
-
-			merge_arr();
-			display(arr);
 		}
+        else if (is_crash(&tmp_other_s) ||is_collide(&tmp_other_s) || is_killed(&tmp_other_s, &tmp_s))
+		{
+            /*
+            if(is_crash(&tmp_other_s))
+                printf("other crash\n");
+            else if(is_collide(&tmp_other_s))
+                printf("other collide\n");
+            else if(is_killed(&tmp_other_s, &tmp_s))
+                printf("other killed\n");
+                */
+			for(int i=0;i<MAP_COL;i++)
+				for(int j=0;j<MAP_ROW;j++)
+					arr[i][j] = -2;	//win
+            write(sock[0], arr, sizeof(char)*MAP_COL*MAP_ROW);
+			for(int i=0;i<MAP_COL;i++)
+				for(int j=0;j<MAP_ROW;j++)
+					arr[i][j] = -3;	//lose
+			write(sock[1], arr, sizeof(char)*MAP_COL*MAP_ROW);
+			break;
+		}
+		else if (is_consume(&tmp_s, &f))
+		{
+			coor tmp = s.arr[s.size-1];
+			move(&s, key);
+			s.arr[s.size++] = tmp;
+        	init_feed(&s,&other_s,&f);
+		}
+        else if (is_consume(&tmp_other_s, &f))
+		{
+			coor tmp = other_s.arr[other_s.size-1];
+			move(&other_s, other_key);
+			other_s.arr[other_s.size++] = tmp;
+        	init_feed(&other_s,&s,&f);
+		}
+		else
+        {
+            move(&s, key);
+            move(&other_s, other_key);
+        }
+
+		merge_arr(arr, map, &s, &other_s, &f);
+        system("clear");
+		display(arr);
+        write(sock[0], arr, sizeof(char)*MAP_COL*MAP_ROW);
+		write(sock[1], arr, sizeof(char)*MAP_COL*MAP_ROW);
 	}
+
+    close(sock);
+    finished = 1;
+}
+void server_init()
+{
+    //서버 소켓 오픈, 주소 초기화
+    int server_sock;
+    struct sockaddr_in server_addr; //IPv4 address structure
+    int addr_size;
+    //IP 주소 받아오기
+    int ip_sock;
+    struct ifreq ifr;
+    char ipstr[40];
+    //억셉트, 클라이언트 소켓
+    int client_socks[2];
+    struct sockaddr_in client_addr; //IPv4 address structure
+    //스레드
+    pthread_t threads;
+    int result_code;
+
+    //소켓 열기
+    server_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_sock == -1)
+    {
+        perror("socket() error");
+        exit(1);
+    }
+
+    //서버 주소 초기화
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET; //Allows IPv4 socket
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);  //모든 주소의 client 접속 가능
+    server_addr.sin_port = htons(GAME_PORT_NUM);   // server socket binding
+
+    //IP주소 받아오기
+    ip_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (ip_sock == -1)
+    {
+        perror("socket() error");
+        exit(1);
+    }
+    strncpy(ifr.ifr_name, IP_ITERFACE_NAME, IFNAMSIZ);
+    if (ioctl(ip_sock, SIOCGIFADDR, &ifr) < 0)
+    {
+        perror("ioctl()_IP error");
+        exit(1);
+    }
+    inet_ntop(AF_INET, ifr.ifr_addr.sa_data+2, ipstr, sizeof(struct sockaddr));
+    printf("IP-Adrress: %s\n", ipstr);
+    close(ip_sock);
+
+    //서버 주소와 소켓 바인드
+    if (bind(server_sock, (struct sockaddr*) &server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("bind() error");
+        exit(1);
+    }
+
+    //클라이언트 리슨
+    if (listen(server_sock, 5) == -1)
+    {
+        perror("listen() error");
+        exit(1);
+    }
+
+	for(int i=0;i<2;i++)
+	{
+	    addr_size = sizeof(client_addr);
+	    client_socks[i] = accept(server_sock, (struct sockaddr*) &client_addr, &addr_size);
+	    if (client_socks[i] == -1)
+	    {
+	        perror("accept() error");
+	        exit(1);
+	    }
+        printf("Connection from: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+	}
+
+	result_code = pthread_create( &threads, NULL, cal, (void*)client_socks);
+	assert(!result_code);
+	result_code = pthread_detach(threads);
+	assert(!result_code);
+
+    while(finished==0);
+    exit(1);
+}
+int main(int argc, char ** argv)
+{
+    if(argc == 1)
+        server_init();
+    else
+    {
+        printf("Usage server: %s\n", argv[0]);
+        exit(1);
+    }
+
+    return 0;
 }
